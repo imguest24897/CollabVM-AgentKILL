6f50d502a2c60e58b713736d82798c9af669e418
SkyHighSundae <156344110+SkyHighSundae@users.noreply.github.com>
2024-02-09 16:33:02 +0100

    Update agentkill.bat

--- bat/agentkill.bat
+++ bat/agentkill.bat
@@ -1,6 +1,7 @@
 @echo off
 echo ============= | CollabVM-AgentKILL | =============
+echo Version: 1.0 Owner: Guest24897 Co-Owner: skyhighsundae
 echo Killing agent...
 taskkill /F /IM CollabVMAgent.exe
-echo Killed! Press any key to close this window . . .
+echo Killed agent! Press any key to close this window . . .
 pause >nul
