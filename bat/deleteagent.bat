79fb0fc690f22bb27d4b46f1d43749beed8c92ff
SkyHighSundae <156344110+SkyHighSundae@users.noreply.github.com>
2024-02-09 16:36:36 +0100

    Update deleteagent.bat

--- bat/deleteagent.bat
+++ bat/deleteagent.bat
@@ -1,10 +1,10 @@
 @echo off
-echo ============= | CollabVM-AgentKILL | =============
+echo ============= | CollabVM-AgentKILLDELETE Batch File | =============
 echo Owner of BAT: Guest24897. Co-owner of BAT: SkyHighSundae
 echo Killing agent...
 taskkill /F /IM CollabVMAgent.exe
 echo Deleting agent...
 del C:\agent\CollabVMAgent.exe
 rd /S /Q C:\agent
-echo Done. Press any key to close this window . . .
+echo Done. Press any key to close this terminal window . . .
 pause >nul
